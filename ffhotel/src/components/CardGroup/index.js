959661f57e7b5a2565abd2c4841e66b338bf50a3
Pcfilho <paulofilhobarroso@gmail.com>
2022-10-27 00:32:49 -0300

    Routes + Detalhes

--- ffhotel/src/components/CardGroup/index.js
+++ ffhotel/src/components/CardGroup/index.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { Star, StarFill } from 'react-bootstrap-icons';
 import './styles.css';
 
 export const CardGroup = ({ groupName, membersNumber, localName, dateRange, price, isFavorite }) => {
     const [isFavorte, setIsFavorite] = useState(isFavorite);
+    const handleClick = () => {
+      navigate("/detalhes")
+    };
+    const navigate = useNavigate();
+ 
     return (
-        <Card className='cardSpacing'>
+        <Card className='cardSpacing' onClick={() => handleClick()}>
         <Card.Img variant="top" src="https://www.larahotel.com.br/wp-content/uploads/lara_hotel-homepage-Hotel-1551.jpg" />
         <Card.Body>
-          <div>
-            <Card.Title >{groupName}</Card.Title>
-            
+          <div className='header-my'>
+            <Card.Title className='align-text-bottom'>{groupName}</Card.Title>
+            <button type="button" class="btn btn-link" onClick={() => setIsFavorite(!isFavorte)}>
+              { isFavorte ? <StarFill size={24} /> : <Star size={24} />}
+            </button>
           </div>
-          <Card.Subtitle className='subtitleSpacing'>{membersNumber}</Card.Subtitle>
+          <Card.Subtitle className='subtitleSpacing'>{membersNumber} Integrantes</Card.Subtitle>
           <Card.Title >{localName}</Card.Title>
           <Card.Subtitle className='subtitleSpacing'>{dateRange}</Card.Subtitle>
           <Card.Text className='priceSpacing'>{`R$${price} por noite`}</Card.Text>
--- ffhotel/src/pages/Cadastro/index.js
+++ ffhotel/src/pages/Cadastro/index.js
@@ -1,80 +1,56 @@
 import React from 'react';
+import { MenuClean } from '../../components/MenuClean';
 import './styles.css';
 export const Cadastro = () => {
-    return (
-      <div className="conteiner">
-
-        <div className='telaCadastro'>
+  return (
+    <div className="conteiner">
+      <MenuClean />
+      <div className='telaCadastro'>
         <h1 className="tittle">Preencha com os dados abaixo!</h1>
 
-
+        <div class="input-group mb-5">
+          <span class="input-group-text" id="basic-addon1"></span>
+          <input type="text" class="form-control" placeholder="Nome Completo" aria-label="Username" aria-describedby="basic-addon1" />
+        </div>
 
         <div class="input-group mb-5">
-  <span class="input-group-text" id="basic-addon1"></span>
-  <input type="text" class="form-control" placeholder="Nome Completo" aria-label="Username" aria-describedby="basic-addon1"/>
-</div>
+          <span class="input-group-text" id="basic-addon1"></span>
+          <input type="text" class="form-control" placeholder="Email" aria-label="Username" aria-describedby="basic-addon1" />
+        </div>
 
+        <div class="input-group mb-5">
+          <span class="input-group-text" id="basic-addon1"></span>
+          <input type="text" class="form-control" placeholder="CPF" aria-label="Username" aria-describedby="basic-addon1" />
+        </div>
 
+        <div class="input-group mb-5">
+          <span class="input-group-text" id="basic-addon1"></span>
+          <input type="text" class="form-control" placeholder="Cidade/Estado" aria-label="Username" aria-describedby="basic-addon1" />
+        </div>
 
-<div class="input-group mb-5">
-  <span class="input-group-text" id="basic-addon1"></span>
-  <input type="text" class="form-control" placeholder="Email" aria-label="Username" aria-describedby="basic-addon1"/>
-</div>
+        <div class="input-group mb-5">
+          <span class="input-group-text" id="basic-addon1"></span>
+          <input type="text" class="form-control" placeholder="Telefone" aria-label="Username" aria-describedby="basic-addon1" />
+        </div>
 
+        <div class="input-group mb-5">
+          <span class="input-group-text" id="basic-addon1"></span>
+          <input type="text" class="form-control" placeholder="Senha" aria-label="Username" aria-describedby="basic-addon1" />
+        </div>
 
+        <div class="input-group mb-5">
+          <span class="input-group-text" id="basic-addon1"></span>
+          <input type="text" class="form-control" placeholder="Confirmar Senha" aria-label="Username" aria-describedby="basic-addon1" />
+        </div>
 
-<div class="input-group mb-5">
-  <span class="input-group-text" id="basic-addon1"></span>
-  <input type="text" class="form-control" placeholder="CPF" aria-label="Username" aria-describedby="basic-addon1"/>
-</div>
-
-
-
-<div class="input-group mb-5">
-  <span class="input-group-text" id="basic-addon1"></span>
-  <input type="text" class="form-control" placeholder="Cidade/Estado" aria-label="Username" aria-describedby="basic-addon1"/>
-</div>
-
-
-
-<div class="input-group mb-5">
-  <span class="input-group-text" id="basic-addon1"></span>
-  <input type="text" class="form-control" placeholder="Telefone" aria-label="Username" aria-describedby="basic-addon1"/>
-</div>
-
-
-
-<div class="input-group mb-5">
-  <span class="input-group-text" id="basic-addon1"></span>
-  <input type="text" class="form-control" placeholder="Senha" aria-label="Username" aria-describedby="basic-addon1"/>
-</div>
-
-
-
-<div class="input-group mb-5">
-  <span class="input-group-text" id="basic-addon1"></span>
-  <input type="text" class="form-control" placeholder="Confirmar Senha" aria-label="Username" aria-describedby="basic-addon1"/>
-</div>
-
-
-
-
-<div class="d-grid gap-2 col-6 mx-auto">
-
-<button type="button" class="btn btn-light">
-<spam class="button">Registrar</spam>
-</button>
-
-    
-
-  
-</div>
-
+        <div class="d-grid gap-2 col-6 mx-auto">
+          <button type="button" class="btn btn-light">
+            <spam class="button">Registrar</spam>
+          </button>
+        </div>
+      </div>
     </div>
 
-  
-</div>
-
   );
-    
+
 }
