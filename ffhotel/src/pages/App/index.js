0a8f061257696174cc39efc6f5e830ac0e8278c6
Japoleca <vitorsampaiond057@gmail.com>
2022-10-27 00:29:26 -0300

    Japa

--- ffhotel/src/pages/App/index.js
+++ ffhotel/src/pages/App/index.js
@@ -1,10 +1,11 @@
 import { Menu } from '../../components/Menu';
+import { Editarperfil } from '../Editarperfil';
 import './styles.css';
 
 export function App() {
   return (
     <div className="App">
-      <Menu />
+      <Editarperfil />
 
     </div>
   );
